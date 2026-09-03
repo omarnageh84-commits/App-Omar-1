32f617f07ce6cfe2e687c7eea13bd617cc3d7173
omarnageh84-commits <omarnageh84@gmail.com>
2026-09-03 11:34:02 +0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE='app-omar-v20-shortcuts';
-const CORE=['./','./index.html?v=20','./daily.html?v=20','./manifest.json?v=20','./icon_192.png','./icon_512.png'];
+const CACHE='app-omar-v21-no-splash';
+const CORE=['./','./index.html?v=21','./daily.html?v=21','./manifest.json?v=21','./icon_192.png','./icon_512.png'];
 self.addEventListener('install',e=>{e.waitUntil(caches.open(CACHE).then(c=>c.addAll(CORE)).then(()=>self.skipWaiting()))});
 self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(k=>Promise.all(k.filter(x=>x!==CACHE).map(x=>caches.delete(x)))).then(()=>self.clients.claim()))});
 self.addEventListener('fetch',e=>{
@@ -11,10 +11,9 @@ self.addEventListener('fetch',e=>{
  }
  e.respondWith(caches.match(e.request).then(m=>m||fetch(e.request).then(r=>{if(r.ok)caches.open(CACHE).then(c=>c.put(e.request,r.clone()));return r;})));
 });
-// للاختصارات الخارجية - عند الضغط على إشعار
 self.addEventListener('notificationclick', e=>{
   e.notification.close();
   const action = e.action || 'daily';
-  let url = './index.html?v=20#'+action;
+  let url = './index.html?v=21#'+action;
   e.waitUntil(clients.openWindow(url));
 });
